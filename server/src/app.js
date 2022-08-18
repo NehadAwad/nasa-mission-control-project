194d0afeefc0f6cd22a076165354e5b7a7b5d13b
NehadAwad <nehadakash622@gmail.com>
2022-08-18 22:25:49 +0600

    bug: route bug fix

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,15 @@ const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
+
+
 app.use(express.json());
-app.use(planetsRouter); 
+// app.use(planetsRouter); 
+
+app.get('/', (req, res) => {
+    res.send({
+        id: 1
+    });
+})
+
 module.export = app;
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -4,6 +4,6 @@ const {
     getAllPlanets 
 } = require('./planets.controller');
 
-planetsRouter.get('planets', getAllPlanets);
+planetsRouter.get('/planets', getAllPlanets);
 
 module.exports = planetsRouter;
