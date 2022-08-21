caa447047738c2df46430510e27b1998d34f5d71
NehadAwad <nehadakash622@gmail.com>
2022-08-21 23:57:08 +0600

    route bug found

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,21 @@
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 const http = require('http');
 const app = require('./app');
+require('dotenv').config();
 
 const server = http.createServer(app);
+
+// const app = require('express')
+// const server = app()
+
+// server.get('/', (req, res) =>{
+//     res.send('ok')
+// })
+
+
+
 
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT} ...`);
 });
+
